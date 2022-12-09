32ed1f232047f716c34b4071c3db3f17251840ff
Craig Gumbley <craiggumbley@gmail.com>
2022-12-09 15:05:17 +0000

    (MAINT) Remove vendor directory

--- vendor/bundle/ruby/2.7.0/gems/protocol-http1-0.14.6/lib/protocol/http1.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2019-2022, by Samuel Williams.
-
-require_relative 'http1/version'
-require_relative 'http1/connection'
